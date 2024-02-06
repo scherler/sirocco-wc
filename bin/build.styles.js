1030005279b4747fd6df8d7a1f56431b6024a309
Devin Nusbaum <dnusbaum@cloudbees.com>
2024-02-06 15:05:34 -0500

    Put Tailwind base CSS in a single file and then reference it from individual style files to avoid duplication
    Signed-off-by: Devin Nusbaum <dnusbaum@cloudbees.com>

--- bin/build.styles.js
+++ bin/build.styles.js
@@ -1,9 +1,9 @@
 const glob = require("glob");
 const path = require('path');
 
-const { sourceCss, config} = require("./config");
+const { sourceCss, srcDir, config} = require("./config");
 
-const buildCss = require("./build.css.js");
+const { buildCss, buildTailwindBaseCss } = require("./build.css.js");
 
 module.exports = (logger) => {
   const styleFiles = glob.sync(sourceCss.replace(/\\/g, "/"));
@@ -13,7 +13,8 @@ module.exports = (logger) => {
   if (!styleFiles.length) {
     throw new Error(sourceCss, "why you no provide files?");
   }
+  buildTailwindBaseCss(srcDir, config, logger);
   styleFiles.forEach((filePath) => {
-    buildCss(filePath, config, logger);
+    buildCss(filePath, srcDir, config, logger);
   });
 };
